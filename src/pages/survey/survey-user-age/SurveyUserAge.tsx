0f017a70d2437366f9de62319d0b77943d668744
hovelopin <kimhojin3714@naver.com>
2022-08-25 00:20:07 +0900

    feat : 유저나이 페이지 완성

--- src/pages/survey/survey-user-age/SurveyUserAge.tsx
+++ src/pages/survey/survey-user-age/SurveyUserAge.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Card from '../../../components/Card';
 
 const SurveyUserAge = () => {
+  const path = '/survey/interest';
   return (
     <UserAgeContainer>
       <UserAgeTitle>나이를 입력해주세요</UserAgeTitle>
-      <UserAgeMiddleContainer>
-        <UserAgeMiddleBtn>중학생</UserAgeMiddleBtn>
-      </UserAgeMiddleContainer>
-      <UserAgeHighContainer>
-        <UserAgeHighBtn>고등학생</UserAgeHighBtn>
-      </UserAgeHighContainer>
+      <Card title="중학생" path={path} />
+      <Card title="고등학생" path={path} />
+      <Card title="대학 재학중" path={path} />
+      <Card title="취업 준비생" path={path} />
+      <Card title="해당 사항 없음" path={path} />
     </UserAgeContainer>
   );
 };
@@ -23,24 +24,5 @@ const UserAgeTitle = styled.h3`
   font-size: 13px;
   text-align: center;
   opacity: 50%;
+  margin-bottom: 40px;
 `;
-const UserAgeMiddleContainer = styled.div`
-  text-align: center;
-`;
-const UserAgeMiddleBtn = styled.button`
-  width: 343px;
-  height: 60px;
-  text-align: center;
-  margin-top: 64.05px;
-  font-size: 15px;
-  background-color: rgb(246, 244, 238);
-  border: 2px solid rgb(246, 244, 238);
-  opacity: 40%;
-  font-weight: 600;
-  cursor: pointer;
-  &:hover {
-    background-color: gray;
-  }
-`;
-const UserAgeHighContainer = styled(UserAgeMiddleContainer)``;
-const UserAgeHighBtn = styled(UserAgeMiddleBtn)``;
