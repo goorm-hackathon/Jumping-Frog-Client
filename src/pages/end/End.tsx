4b3d47bf170fe02c38577c74c44aef3f2edaabd0
hovelopin <kimhojin3714@naver.com>
2022-08-24 22:17:46 +0900

    feat : 엔드페이지 및 글로벌 스타일 변경

--- src/pages/end/End.tsx
+++ src/pages/end/End.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const End = () => {
-  return <div>End</div>;
+  return (
+    <EndContainer>
+      <EndContent>
+        제출해주셔서 감사해요 <br />
+        제출해주셔서 감사해요 <br />
+        제출해주셔서 감사해요 <br />
+      </EndContent>
+    </EndContainer>
+  );
 };
 
 export default End;
+
+const EndContainer = styled.div``;
+const EndContent = styled.p`
+  margin-top: 184px;
+  text-align: center;
+  line-height: 50px;
+`;
