10a94fc73ffffaa414a280c8f090a30f0a432f2c
hovelopin <kimhojin3714@naver.com>
2022-08-25 00:09:13 +0900

    feat : 유저 나이 페이지 임시 커밋

--- src/pages/survey/survey-user-age/SurveyUserAge.tsx
+++ src/pages/survey/survey-user-age/SurveyUserAge.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const SurveyUserAge = () => {
-  return <div>유저 나이</div>;
+  return (
+    <UserAgeContainer>
+      <UserAgeTitle>나이를 입력해주세요</UserAgeTitle>
+      <UserAgeMiddleContainer>
+        <UserAgeMiddleBtn>중학생</UserAgeMiddleBtn>
+      </UserAgeMiddleContainer>
+      <UserAgeHighContainer>
+        <UserAgeHighBtn>고등학생</UserAgeHighBtn>
+      </UserAgeHighContainer>
+    </UserAgeContainer>
+  );
 };
 
 export default SurveyUserAge;
+
+const UserAgeContainer = styled.div``;
+const UserAgeTitle = styled.h3`
+  margin-top: 31.68px;
+  font-size: 13px;
+  text-align: center;
+  opacity: 50%;
+`;
+const UserAgeMiddleContainer = styled.div`
+  text-align: center;
+`;
+const UserAgeMiddleBtn = styled.button`
+  width: 343px;
+  height: 60px;
+  text-align: center;
+  margin-top: 64.05px;
+  font-size: 15px;
+  background-color: rgb(246, 244, 238);
+  border: 2px solid rgb(246, 244, 238);
+  opacity: 40%;
+  font-weight: 600;
+  cursor: pointer;
+  &:hover {
+    background-color: gray;
+  }
+`;
+const UserAgeHighContainer = styled(UserAgeMiddleContainer)``;
+const UserAgeHighBtn = styled(UserAgeMiddleBtn)``;
