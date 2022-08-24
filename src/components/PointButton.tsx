63a21e81d85e63df9c8cc169567546f0daf63181
hovelopin <kimhojin3714@naver.com>
2022-08-25 01:27:59 +0900

    feat : 마켓 페이지 임시 완성

---
+++ src/components/PointButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PointButton = ({ point }: { point: string }) => {
+  return <SurveyPointButton>{point}</SurveyPointButton>;
+};
+
+export default PointButton;
+
+const SurveyPointButton = styled.button`
+  text-align: center;
+  width: 62px;
+  height: 55px;
+  background-color: #f6f4ee;
+  border: 2px solid rgb(246, 244, 238);
+  opacity: 40%;
+  font-weight: 600;
+  cursor: pointer;
+  border-radius: 12px;
+`;
--- src/pages/survey/survey-market/SurveyMarket.tsx
+++ src/pages/survey/survey-market/SurveyMarket.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import PointButton from '../../../components/PointButton';
+import SurveySubTitle from '../../../components/SurveySubTitle';
+import SurveyTitle from '../../../components/SurveyTitle';
 
 const SurveyMarket = () => {
-  return <div>Market</div>;
+  return (
+    <>
+      <SurveyTitle content="당신의 성향을 체크해주세요." />
+      <SurveySubTitle content="나는 현재 제주도에 취업시장이 다양한 직업군에 접근할 수 있도록 활성화 되어 있다고 생각해요" />
+      <PointButtonContainer>
+        <PointButton point="1" />
+        <PointButton point="2" />
+        <PointButton point="3" />
+        <PointButton point="4" />
+        <PointButton point="5" />
+      </PointButtonContainer>
+    </>
+  );
 };
 
 export default SurveyMarket;
+
+const PointButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
