53a12068315f8af7e649869b001aa298e896dda1
Jaehyeok <milkboy2564@naver.com>
2022-08-25 15:05:48 +0900

    feat: recoil state

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState, useResetRecoilState } from 'recoil';
+import userAtom from '../recoil/userAtom';
 
-const Card = ({ title, path }: { title: string; path: string }) => {
+const Card = ({
+  flag,
+  content,
+  path,
+}: {
+  flag: string;
+  content: string;
+  path: string;
+}) => {
   const navigate = useNavigate();
-
-  const handleClick = () => {
+  const [userData, setUserData] = useRecoilState(userAtom);
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const { value } = e.currentTarget;
+    setUserData({
+      ...userData,
+      flag: value,
+    });
     navigate(path);
   };
+  console.log(userData);
 
   return (
     <CardContainer>
-      <CardBtn onClick={handleClick}>{title}</CardBtn>
+      <CardBtn onClick={handleClick} value={content}>
+        {content}
+      </CardBtn>
     </CardContainer>
   );
 };
--- src/pages/survey/survey-user-gender/SurveyUserGender.tsx
+++ src/pages/survey/survey-user-gender/SurveyUserGender.tsx
@@ -9,8 +9,8 @@ const SurveyUserGender = () => {
     <>
       <Progress percent={20} />
       <SurveyTitle content="성별을 입력해주세요" />
-      <Card title="남자" path={path} />
-      <Card title="여자" path={path} />
+      <Card flag="gender" content="남자" path={path} />
+      <Card flag="gender" content="여자" path={path} />
     </>
   );
 };
