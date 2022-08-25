8c6ccc4d8c1c810d8b1060c110ca70d4660fdbda
hovelopin <kimhojin3714@naver.com>
2022-08-25 11:18:44 +0900

    feat : 직업 페이지 완성

--- src/pages/survey/survey-job/SurveyJob.tsx
+++ src/pages/survey/survey-job/SurveyJob.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import PointButton from '../../../components/PointButton';
+import SurveySubTitle from '../../../components/SurveySubTitle';
+import SurveyTitle from '../../../components/SurveyTitle';
+import {
+  PointButtonContainer,
+  SurveyPointImg,
+  SurveyPointImgContainer,
+  SurveyPointOpinionContainer,
+  SurveyPointOpinionNo,
+  SurveyPointOpinionYes,
+} from '../survey-market/SurveyMarket';
 
 const SurveyJob = () => {
-  return <div>Job</div>;
+  return (
+    <>
+      <SurveyTitle content="당신의 성향을 체크해주세요." />
+      <SurveySubTitle content="나는 현재 다양한 직업에 대해 알고 있다고 느껴요" />
+      <SurveyPointOpinionContainer>
+        <SurveyPointOpinionNo>전혀 그렇지 않다.</SurveyPointOpinionNo>
+        <SurveyPointOpinionYes>매우 그렇다.</SurveyPointOpinionYes>
+      </SurveyPointOpinionContainer>
+      <SurveyPointImgContainer>
+        <SurveyPointImg
+          src={require('../../../assets/images/rectangle.png')}
+          alt="로고"
+        />
+      </SurveyPointImgContainer>
+      <PointButtonContainer>
+        <PointButton point="1" />
+        <PointButton point="2" />
+        <PointButton point="3" />
+        <PointButton point="4" />
+        <PointButton point="5" />
+      </PointButtonContainer>
+    </>
+  );
 };
 
 export default SurveyJob;
