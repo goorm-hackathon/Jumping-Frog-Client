d212ccac85bc2362b6c9432d7e71fb2362ac8db6
hovelopin <kimhojin3714@naver.com>
2022-08-26 02:35:45 +0900

    feat : 디테일 스타일 수정

--- src/components/SurveySubTitle.tsx
+++ src/components/SurveySubTitle.tsx
@@ -8,7 +8,9 @@ const SurveySubTitle = ({ content }: { content: string }) => {
 export default SurveySubTitle;
 
 const SurveySubContent = styled.h2`
-  opacity: 40%;
+  opacity: 50%;
   text-align: center;
   margin-top: 77px;
+  font-size: 14px;
+  font-weight: 600;
 `;
