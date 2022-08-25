0355066323ee62757790571fce263e15390e7481
hovelopin <kimhojin3714@naver.com>
2022-08-25 23:32:37 +0900

    feat : home 컴포넌트 추가

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Home = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/main');
+    }, 3000);
+  });
+  return (
+    <HomeContainer>
+      <HomeImg src={require('../../assets/images/home2.gif')} alt="home" />
+    </HomeContainer>
+  );
+};
+
+export default Home;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  /* height: 100%; */
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+const HomeImg = styled.img`
+  width: 100%;
+`;
